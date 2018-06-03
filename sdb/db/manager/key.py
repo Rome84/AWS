94adcf68ca4a0f4b594ce7372fd4ccae6093a139
GEROME84 <uszone3@gmail.com>
2018-06-03 15:01:55 -0400

    Add files via upload

--- sdb/db/manager/key.py
+++ sdb/db/manager/key.py
@@ -14,7 +14,46 @@
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
 # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
 # ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
-# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
 # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
+
+class Key(object):
+
+    @classmethod
+    def from_path(cls, *args, **kwds):
+        raise NotImplementedError("Paths are not currently supported")
+
+    def __init__(self, encoded=None, obj=None):
+        self.name = None
+        if obj:
+            self.id = obj.id
+            self.kind = obj.kind()
+        else:
+            self.id = None
+            self.kind = None
+
+    def app(self):
+        raise NotImplementedError("Applications are not currently supported")
+
+    def kind(self):
+        return self.kind
+
+    def id(self):
+        return self.id
+
+    def name(self):
+        raise NotImplementedError("Key Names are not currently supported")
+
+    def id_or_name(self):
+        return self.id
+
+    def has_id_or_name(self):
+        return self.id is not None
+
+    def parent(self):
+        raise NotImplementedError("Key parents are not currently supported")
+
+    def __str__(self):
+        return self.id_or_name()
